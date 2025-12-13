97020456d3bcb71b6c9012cb06e2ff54767ad037
rifg67 <kamprethsuwhsh@gmail.com>
2025-12-14 06:31:56 +0700

    Add files via upload

--- sshws/ohp.sh
+++ sshws/ohp.sh
@@ -15,7 +15,7 @@ LIGHT='\033[0;37m'
 # Getting
 
 # Download File Ohp
- wget -O /usr/local/bin/ohpserver "http://raw.githubusercontent.com/rifg67/script-rifts/main/sshws/ohpserver"
+ wget -O /usr/local/bin/ohpserver "https://raw.githubusercontent.com/rifg67/script-rifts/main/sshws/ohpserver"
  chmod +x /usr/local/bin/ohpserver
 # Installing Service
 # SSH OHP Port 8181
